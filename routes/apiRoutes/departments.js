2df968b1f4621d95dce5928b492f245bd723ffed
Abby Roberg <abbyroberg@gmail.com>
2021-09-16 17:08:08 -0500

    add department route

--- routes/apiRoutes/departments.js
+++ routes/apiRoutes/departments.js
@@ -18,4 +18,21 @@ router.get('/departments', (req, res) => {
     })
 })
 
+// add a department
+router.post('/departments', ({ body }, res) => {
+    const sql = `INSERT INTO departments (name) VALUES (?)`
+    const params = [body.name]
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message })
+            return
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })
+    })
+})
+
 module.exports = router
