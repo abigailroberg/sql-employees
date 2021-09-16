a9df64b31e9ac0214b68b2894529bea0d35cb568
Abby Roberg <abbyroberg@gmail.com>
2021-09-16 18:01:44 -0500

    update employee role route

--- apiRoutes.js
+++ apiRoutes.js
@@ -107,7 +107,9 @@ router.get('/employees', (req, res) => {
 
     // add an employee
 router.post('/employees', ({ body }, res) => {
-    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`
+    const sql = 
+    `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+        VALUES (?, ?, ?, ?)`
     const params = [body.first_name, body.last_name, body.role_id, body.manager_id]
     
     db.query(sql, params, (err, result) => {
@@ -121,5 +123,28 @@ router.post('/employees', ({ body }, res) => {
         })
     })
 })
+
+    // update employee role
+router.put('/employees/:id', (req, res) => {  
+    const sql = `UPDATE employees SET role_id = ? 
+                 WHERE id = ?`
+    const params = [req.body.role_id, req.params.id]
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found'
+        })
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        })
+      }
+    })
+  })
 
 module.exports = router
