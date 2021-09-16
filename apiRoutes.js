3eaf6f81eec3c574379fe1e25314b30db7f03aff
Abby Roberg <abbyroberg@gmail.com>
2021-09-16 17:20:44 -0500

    view all roles route

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../../db/connection')
+const db = require('./db/connection')
 
-// view all departments
+// department routes
+    // view all departments
 router.get('/departments', (req, res) => {
     const sql = `SELECT * FROM departments`
 
@@ -18,7 +19,7 @@ router.get('/departments', (req, res) => {
     })
 })
 
-// add a department
+    // add a department
 router.post('/departments', ({ body }, res) => {
     const sql = `INSERT INTO departments (name) VALUES (?)`
     const params = [body.name]
@@ -31,6 +32,30 @@ router.post('/departments', ({ body }, res) => {
         res.json({
             message: 'success',
             data: body
+        })
+    })
+})
+
+// role routes
+    // view all roles
+router.get('/roles', (req, res) => {
+    const sql = 
+    `SELECT roles.title, 
+        roles.id AS role_id,
+        departments.name AS department,
+        roles.salary
+    FROM roles
+    JOIN departments
+    ON roles.department_id = departments.id`
+
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message })
+            return
+        }
+        res.json({
+            message: 'success',
+            data: rows
         })
     })
 })
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const db = require('./db/connection')
 const express = require('express')
-const apiRoutes = require('./routes/apiRoutes/departments')
+const apiRoutes = require('./apiRoutes')
 
 const PORT = process.env.PORT || 3001
 const app = express()
